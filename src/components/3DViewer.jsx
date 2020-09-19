a0acbd19ce4ed7c32b08bed2c34b631e55eae75f
unknown <taveras574@gmail.com>
2020-09-18 20:04:27 -0400

    The game of life is working

--- src/components/3DViewer.jsx
+++ src/components/3DViewer.jsx
@@ -7,8 +7,8 @@ class Viewer extends Component {
     super(props);
     this.state = {
       grid: null,
-      cols: 100,
-      rows: 100,
+      cols: 40,
+      rows: 40,
       scene: null,
       width: 0,
       height: 0,
@@ -36,16 +36,16 @@ class Viewer extends Component {
 
   initialize() {
     this.state.grid = this.make2DArray(this.state.cols, this.state.rows);
-    this.state.scene =  new THREE.Scene();
+    this.state.scene = new THREE.Scene();
     this.state.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.state.renderer.setClearColor(0x00000);
     this.state.renderer.setSize(this.state.width, this.state.height);
     this.mount.append(this.state.renderer.domElement)
-    this.state.camera = new THREE.PerspectiveCamera(75, this.state.width / this.state.height, 0.1, 1000);
+    this.state.camera = new THREE.PerspectiveCamera(30, this.state.width / this.state.height, 0.1, 1000);
     new OrbitControls(this.state.camera, this.state.renderer.domElement);
-    this.state.camera.position.z = 150;
+    this.state.camera.position.z = 100;
     this.state.camera.position.y = -150;
-    this.state.camera.position.x = 150;
+    this.state.camera.position.x = 50;
     this.state.camera.rotateX(1);
     this.state.camera.rotateY(0);
     this.state.camera.rotateZ(45);
@@ -60,16 +60,16 @@ class Viewer extends Component {
     this.state.scene.add(lights[0]);
     this.state.scene.add(lights[1]);
     this.state.scene.add(lights[2]);
-    
+
     for (let i = 0; i < this.state.cols; i++) {
       for (let j = 0; j < this.state.rows; j++) {
-        this.state.grid[i][j] = Math.round(Math.random(2))
+        this.state.grid[i][j] = Math.round(Math.random())
       }
     }
 
     this.draw()
     this.renderScene();
-    // this.startAnimation()
+    this.startAnimation()
 
 
   }
@@ -83,37 +83,81 @@ class Viewer extends Component {
     return arr
   }
 
-  draw(){
+  draw = () => {
+
+   
+
     for (let i = 0; i < this.state.cols; i++) {
       for (let j = 0; j < this.state.rows; j++) {
-        if (this.state.grid[i][j] === 1){
-          continue
+        if (this.state.grid[i][j] === 1) {
+          const shape = new THREE.BoxBufferGeometry(1, 1, 1);
+          const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+          let cube = new THREE.Mesh(shape, material);
+          cube.name = `cube-${i}-${j}`
+          cube.position.x = i + i;
+          cube.position.y = j + j;
+          this.state.scene.add(cube);
+        } else {
+
+          this.state.scene.remove(this.state.scene.getObjectByName(`cube-${i}-${j}`))
         }
-        const shape = new THREE.BoxBufferGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({color:  0xffffff});
-        let cube = new THREE.Mesh(shape, material);
-        cube.position.x = i + i;
-        cube.position.y = j + j;
-        this.state.scene.add(cube);
       }
     }
 
+    let next = this.make2DArray(this.state.cols, this.state.rows);
+
+    for (let i = 0; i < this.state.cols; i++) {
+      for (let j = 0; j < this.state.rows; j++) {
+        let state = this.state.grid[i][j];
+        // Count live neighbors!
+        let sum = 0;
+        let neighbors = this.countNeighbors(this.state.grid, i, j);
+
+        if (state == 0 && neighbors == 3) {
+          next[i][j] = 1;
+        } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+          next[i][j] = 0;
+        } else {
+          next[i][j] = state;
+        }
+      }
+    }
+
+    this.state.grid = next
+
   }
-  
-  startAnimation(){
-    if (!this.state.frameId){
+
+  startAnimation = () => {
+    if (!this.state.frameId) {
       this.state.frameId = requestAnimationFrame(this.animate)
     }
-    
+
   }
-  animate(){
+
+  countNeighbors = (grid, x, y) => {
+    let sum = 0;
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        let col = (x + i + this.state.cols) % this.state.cols;
+        let row = (y + j + this.state.rows) % this.state.rows;
+        sum += grid[col][row];
+      }
+    }
+    sum -= this.state.grid[x][y];
+    return sum;
+  }
+
+  animate = () => {
+    this.draw()
+
     this.renderScene();
+
     this.state.frameId = window.requestAnimationFrame(this.animate)
   }
-  
-  renderScene(){
-    if(this.state.renderer) this.state.renderer.render(this.state.scene, this.state.camera)
-    
+
+  renderScene = () => {
+    if (this.state.renderer) this.state.renderer.render(this.state.scene, this.state.camera)
+
   }
 
 
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,77 +2,7 @@ import React, { Component } from 'react';
 
 import Viewer from '../components/3DViewer';
 
-import { OBJLoader } from 'three-obj-mtl-loader';
-
 class Home extends Component {
-  componentDidMount() {
-    // const width = this.mount.clientWidth;
-    // const height = this.mount.clientHeight;
-    // this.scene = new THREE.Scene();
-
-    // this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    // this.renderer.setClearColor('#00000');
-    // this.renderer.setSize(width, height);
-    // this.mount.appendChild(this.renderer.domElement);
-
-    // this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    // new OrbitControls(this.camera, this.renderer.domElement);
-    // this.camera.position.z = 150;
-    // this.camera.position.y = -150;
-    // this.camera.position.x = 150;
-    // this.camera.rotateX(1);
-    // this.camera.rotateY(0);
-    // this.camera.rotateZ(45);
-
-    // var lights = [];
-    // lights[0] = new THREE.PointLight(0x304ffe, 1, 0);
-    // lights[1] = new THREE.PointLight(0xffffff, 1, 0);
-    // lights[2] = new THREE.PointLight(0xffffff, 1, 0);
-    // lights[0].position.set(0, 200, 0);
-    // lights[1].position.set(100, 200, 100);
-    // lights[2].position.set(-100, -200, -100);
-    // this.scene.add(lights[0]);
-    // this.scene.add(lights[1]);
-    // this.scene.add(lights[2]);
-
-    // this.gridSize = 100;
-
-    // // for (let i = 0; i < this.gridSize; i++) {
-    // //   for (let j = 0; j < this.gridSize; j++) {
-    // //     const shape = new THREE.BoxBufferGeometry(1, 1, 1);
-    // //     const material = new THREE.MeshBasicMaterial({ color: 0xfc8f34 });
-    // //     let cube = new THREE.Mesh(shape, material);
-    // //     cube.position.x = i + i;
-    // //     cube.position.y = j + j;
-    // //     this.scene.add(cube);
-    // //   }
-    // // }
-    
-    // this.renderScene();
-    // this.start();
-  }
-  start = () => {
-    if (!this.frameId) {
-      this.frameId = requestAnimationFrame(this.animate);
-    }
-  };
-  stop = () => {
-    cancelAnimationFrame(this.frameId);
-  };
-  animate = () => {
-    if (this.brian) this.brian.rotation.y += 0.01;
-
-    this.renderScene();
-    this.frameId = window.requestAnimationFrame(this.animate);
-  };
-  renderScene = () => {
-    if (this.renderer) this.renderer.render(this.scene, this.camera);
-  };
-
-
-  create2DArray(cols, rows){
-
-  }
 
   render() {
     return (
